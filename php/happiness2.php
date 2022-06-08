922201e64a09b00ea86ec730b24c646344b4939d
LoicKalai <loic.kalaidjoglou@icloud.com>
2022-06-08 09:57:45 +0200

    [Add]
    Ajout d'une deuxième page pour refont en mobile first

---
+++ php/happiness2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://kit.fontawesome.com/75892227ad.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" href="../css/happiness.css">
+
+	<title>Happy Health</title>
+</head>
+<body>
+    <main class="bg-[#200355] h-screen w-screen text-stone-50">
+        <nav class="w-full flex lg:justify-between lg:items-center lg:h-24 h-20 font-bold">
+            <div class="flex mt-5 lg:ml-32 md:ml-16 ">
+                <img class="mt-1 ml-4 mr-16 h-7" src="../assets/media/logo.png">
+                <h2  class="text-xl h2nav invisible md:visible lg:visible ">Happy health</h2>
+            </div>
+
+            <div class="flex mt-6 text-xl mr-14 md:ml-32">
+				<a class="mr-10 text-sm font text-[#FEC505]" href="#">Happy</a>
+				<a class="text-sm hover:text-[#44b562]" href="#">Health</a>
+			</div>
+        </nav>
+    </main>
+    
+
+
+
+    <script type="text/javascript" src="../js/advice.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<script type="text/javascript" src="../js/chart.js"></script>
+    <script src="../js/chart2.js"></script>
+</body>
+</html>
