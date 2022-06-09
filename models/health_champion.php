f3c7caa47e47648aad32028b2ff6279b7981416b
ElenaGir <elena.girard@gmail.com>
2022-06-09 15:50:11 +0200

    add req page champion of health

---
+++ models/health_champion.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('connect.php');
+    
+    $selec_year = $_POST["selec_year"];
+    $sth = $bdd->prepare('SELECT country, MAX(`value`) AS val_max, year FROM countries_has_years
+    INNER JOIN countries ON countries_has_years.countries_id = countries.id
+    INNER JOIN `values` ON  values.id = countries_has_years.health
+    INNER JOIN years ON  countries_has_years.years_id = years.id
+    WHERE year = :year
+    GROUP BY country
+    ORDER BY val_max DESC');
+
+    $sth->bindValue('', $selec_year, PDO::PARAM_INT);
+    $sth->execute();
+    $res = $sth->fetch(PDO::FETCH_ASSOC);
+    
+    echo json_encode($res);
+
+?>
