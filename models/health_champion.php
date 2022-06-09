19f390a07c41d82bd3372fb7f9e07a155876cd96
ElenaGir <elena.girard@gmail.com>
2022-06-09 16:51:14 +0200

    add req page champion of health

--- models/health_champion.php
+++ models/health_champion.php
@@ -2,15 +2,16 @@
     require_once('connect.php');
     
     $selec_year = $_POST["selec_year"];
-    $sth = $bdd->prepare('SELECT country, MAX(`value`) AS val_max, year FROM countries_has_years
-    INNER JOIN countries ON countries_has_years.countries_id = countries.id
+    $sth = $bdd->prepare('SELECT country, MAX(`value`) AS val_max, `year` FROM countries_has_years
+    INNER JOIN countries ON  countries.id = countries_has_years.countries_id
     INNER JOIN `values` ON  values.id = countries_has_years.health
-    INNER JOIN years ON  countries_has_years.years_id = years.id
-    WHERE year = :year
+    INNER JOIN years ON  years.id = countries_has_years.years_id 
+    WHERE `year` = :year
     GROUP BY country
-    ORDER BY val_max DESC');
+    ORDER BY val_max DESC
+    LIMIT 1');
 
-    $sth->bindValue('', $selec_year, PDO::PARAM_INT);
+    $sth->bindValue(':year', $selec_year, PDO::PARAM_INT);
     $sth->execute();
     $res = $sth->fetch(PDO::FETCH_ASSOC);
     
