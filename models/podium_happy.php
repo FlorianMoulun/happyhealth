6a210f56655bed1af1497ea31d6ab73d294b3ba0
Moulun Florian <flomoulun@gmx.fr>
2022-06-09 16:46:23 +0200

    [ADD]
    Ajout de la requete pour afficher les 3 meilleurs pays

---
+++ models/podium_happy.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Connexion pdo */
+require_once('connect.php');
+
+$sth = $bdd->prepare('SELECT country, `year`, value FROM countries_has_years
+                        INNER JOIN countries ON countries_id = countries.id
+                        INNER JOIN years ON years_id = years.id
+                        INNER JOIN `values` ON happiness_rank = values.id
+                        WHERE `year` = 2022
+                        ORDER BY happiness_rank
+                        LIMIT 3');
+
+$sth->bindValue(':year', 2019, PDO::PARAM_INT);
+$sth->execute();
+$res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+json_encode($res);
