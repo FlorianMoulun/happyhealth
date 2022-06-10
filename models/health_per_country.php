f78b3e5d5a2c3c7b8bae5ac933640f69ef60e114
Moulun Florian <flomoulun@gmx.fr>
2022-06-10 11:32:09 +0200

    [ADD]
    Ajout de la requete pour le graphe health per country

---
+++ models/health_per_country.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Connexion pdo */
+require_once('connect.php');
+
+$sth = $bdd->prepare('SELECT ROUND(AVG(value), 2), country, `year` FROM countries_has_years
+                        INNER JOIN `values` ON health = values.id
+                        INNER JOIN years ON years_id = years.id
+                        INNER JOIN countries ON countries_id = countries.id
+                        WHERE country = :name_country
+                        GROUP BY `year`
+                        ');
+
+$sth->bindValue(':year', 2019, PDO::PARAM_INT);
+$sth->execute();
+$res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+json_encode($res);
