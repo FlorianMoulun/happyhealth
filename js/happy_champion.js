e9b8c0b05d4b1019e25d2b5a03e112ff5c56d57d
ElenaGir <elena.girard@gmail.com>
2022-06-10 14:52:45 +0200

    arrows function

--- js/happy_champion.js
+++ js/happy_champion.js
@@ -1,6 +1,6 @@
 let count_year = 2022;
  const form = new FormData();
- form.append("select_year", count_year );
+ form.append("select_year", count_year);
  
  fetch('./models/happy_champion.php',{
      method: 'POST',
@@ -22,11 +22,8 @@ let count_year = 2022;
 
 const arrow_happy_left = document.querySelector('#arrow_happy_left');
 const arrow_happy_right = document.querySelector('#arrow_happy_right');
-console.log(arrow_happy_right);
-
 
 // bouton  left
-
   arrow_happy_left.addEventListener('click',()=>{
     // creation de faux-formulaire 
     const form = new FormData();
@@ -59,17 +56,16 @@ console.log(arrow_happy_right);
 })
 
 arrow_happy_right.addEventListener('click',()=>{
+    
     // creation de faux-formulaire 
-   let year_happy = parseInt(document.querySelector(`#year_happy`).firstChild.nodeValue);
-    console.log(year_happy);
     const form = new FormData();
-    if (year_happy == 2021 || year_happy == 2015 || year_happy == 2016 || year_happy == 2017 || year_happy == 2018 || year_happy == 2019 || year_happy == 2020) {
-        year_happy ++;
+    if (count_year == 2021 || count_year == 2015 || count_year == 2016 || count_year == 2017 || count_year == 2018 || count_year == 2019 || count_year == 2020) {
+        count_year ++;
     }else{
-        year_happy= 2015;
+        count_year= 2015;
     }   
    
-    form.append("select_year", year_happy);
+    form.append("select_year", count_year);
     
     fetch('./models/happy_champion.php',{
         method: 'POST',
