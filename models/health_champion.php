b6d7e0fc138915dc0c43975d7eadbc301f5c86ed
ElenaGir <elena.girard@gmail.com>
2022-06-10 15:46:58 +0200

    round value

--- models/health_champion.php
+++ models/health_champion.php
@@ -2,7 +2,7 @@
     require_once('connect.php');
     
     $selec_year = $_POST["select_year"];
-    $sth = $bdd->prepare('SELECT country, MAX(`value`) AS val_max, `year` FROM countries_has_years
+    $sth = $bdd->prepare('SELECT country, ROUND(MAX(`value`), 2) AS val_max, `year` FROM countries_has_years
     INNER JOIN countries ON  countries.id = countries_has_years.countries_id
     INNER JOIN `values` ON  values.id = countries_has_years.health
     INNER JOIN years ON  years.id = countries_has_years.years_id 
