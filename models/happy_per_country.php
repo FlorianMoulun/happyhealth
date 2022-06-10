98a4d840b0ac6662ce47be66170611c5dd88870c
Moulun Florian <flomoulun@gmx.fr>
2022-06-10 11:35:57 +0200

    [ADD]
    Ajout de la requete pour le graphe happy per country and fixed requete pour le graph health per country

--- models/happy_per_country.php
+++ models/happy_per_country.php
@@ -4,7 +4,7 @@
 require_once('connect.php');
 
 $sth = $bdd->prepare('SELECT ROUND(AVG(value), 2), country, `year` FROM countries_has_years
-                        INNER JOIN `values` ON health = values.id
+                        INNER JOIN `values` ON happiness_score = values.id
                         INNER JOIN years ON years_id = years.id
                         INNER JOIN countries ON countries_id = countries.id
                         WHERE country = :name_country
